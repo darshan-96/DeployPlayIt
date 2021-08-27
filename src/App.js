fae5d5de9979d68844a9e71b54f05e0d4ced6afa
Darshan G <62536522+darshan-96@users.noreply.github.com>
2021-08-27 15:33:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 function App() {
   return (
     <div className="App">
-      <ReactPlayer controls url="https://edge.stg-alefedge.com/v1/content?url=00c104e612cb7f461e6446d77125a0edb3b8a827df1bd1149f3a08cad05d4af7330bc06d85d2da3b753f00c9f4d3be95e4638711ca83c62bdbd0a0bd3d143d5e&api_key=6S5F6S6-4JZMFEG-KAJVWQ6-B5Q6117" playing="true"></ReactPlayer>
+      <ReactPlayer controls url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" playing="true"></ReactPlayer>
       {/* https://edge.stg-alefedge.com/v1/content?url=00c104e612cb7f461e6446d77125a0edb3b8a827df1bd1149f3a08cad05d4af7330bc06d85d2da3b753f00c9f4d3be95e4638711ca83c62bdbd0a0bd3d143d5e&api_key=6S5F6S6-4JZMFEG-KAJVWQ6-B5Q6117 */}
     </div>
   );
